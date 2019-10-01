decf845fe87e0818b5af10be6277e64da10ed1a8
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2019-10-01 15:30:32 +0200

    minor doc

--- inet/doc.go
+++ inet/doc.go
@@ -45,7 +45,18 @@ Items abstracted via Itemer interface
  	Childs []*Node
  }
 
-The tree can be visualized as:
+Itemer interface for tree items, maybe with payload and not just ip Blocks.
+See relation between Compare and Contains at inet.Block.Compare()
+ Itemer interface {
+ 
+ 	// Contains, defines the depth in the tree, parent child relationship.
+ 	Contains(Itemer) bool
+ 
+ 	// Compare, defines equality and sort order on same tree level, siblings relationship.
+ 	Compare(Itemer) int
+ }
+
+The trie can be visualized as:
 
  ▼
  ├─ 10.0.0.0/9
