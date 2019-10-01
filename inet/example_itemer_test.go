e31b3c2e5cc42070a9ce39238eb499967f71c911
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2019-10-01 22:27:11 +0200

    wip

--- inet/example_itemer_test.go
+++ inet/example_itemer_test.go
@@ -8,59 +8,79 @@ import (
 	"github.com/gaissmai/go-inet/inet"
 )
 
-type stringItem string
+// record implements inet.Itemer
+type record struct {
+	cidr inet.Block
+	txt  string
+}
 
-func (a stringItem) Contains(b inet.Itemer) bool {
-	c, ok := b.(stringItem)
+// Contains, part of the Itemer interface
+func (a record) Contains(b inet.Itemer) bool {
+	c, ok := b.(record)
 	if !ok {
 		panic(fmt.Errorf("incompatible types: %T != %T", a, b))
 	}
-	// e.g. a = "foo", b=c="foobar"; a contains b in the sense of a trie
-	return strings.Index(string(c), string(a)) == 0
+	return a.cidr.Contains(c.cidr) ||
+		(a.cidr.Compare(c.cidr) == 0 && strings.Compare(a.txt, c.txt) < 0)
 }
 
-func (a stringItem) Compare(b inet.Itemer) int {
-	c, ok := b.(stringItem)
+// Compare, part of the Itemer interface
+func (a record) Compare(b inet.Itemer) int {
+	c, ok := b.(record)
 	if !ok {
 		panic(fmt.Errorf("incompatible types: %T != %T", a, b))
 	}
-	return strings.Compare(string(a), string(c))
+
+	cmp := a.cidr.Compare(c.cidr)
+	if cmp != 0 {
+		return cmp
+	}
+	return strings.Compare(a.txt, c.txt)
 }
 
-func ExampleItemer_stringItem() {
+// String implements the Stringer interface
+func (a record) String() string {
+	cidr := a.cidr.String()
+	spaceDots := 50 - len(cidr)
+	spacer := strings.Repeat(".", spaceDots)
+	return fmt.Sprintf("%s %s %s", cidr, spacer, a.txt)
+}
+
+func ExampleItemer_interface() {
 	tr := inet.NewTrie()
-
-	for _, s := range []string{
-		"ha",
-		"hallo",
-		"f",
-		"ha", // duplicate
-		"barber shop",
-		"hallo", // duplicate
-		"foo_bar",
-		"bar",
-		"foo",
-		"barbara",
-		"fo",
+	for _, r := range []record{
+		record{inet.MustBlock("0.0.0.0/8"), "lorem"},
+		record{inet.MustBlock("1.0.0.0/8"), "lorem"},
+		record{inet.MustBlock("::/64"), "lorem"},
+		record{inet.MustBlock("5.0.0.0/8"), "lorem"},
+		record{inet.MustBlock("0.0.0.0/0"), "lorem"},
+		record{inet.MustBlock("10.0.0.0-10.0.0.17"), "lorem"},
+		record{inet.MustBlock("::/0"), "same cidr"},
+		record{inet.MustBlock("::/0"), "same cidr, different text"},
+		record{inet.MustBlock("2001:7c0:900:1c2::/64"), "lorem"},
+		record{inet.MustBlock("2001:7c0:900:1c2::0/127"), "lorem"},
+		record{inet.MustBlock("2001:7c0:900:1c2::1/128"), "lorem"},
+		record{inet.MustBlock("0.0.0.0/10"), "lorem"},
+		// ...
 	} {
-		tr.Insert(stringItem(s))
+		tr.Insert(r)
 	}
 
 	tr.Fprint(os.Stdout)
 
-/*
-
-▼
-├─ bar
-│  ├─ barbara
-│  └─ barber shop
-├─ f
-│  └─ fo
-│     └─ foo
-│        └─ foo_bar
-└─ ha
-	 └─ hallo
-
-*/
+	// Output:
+	// ▼
+	// ├─ 0.0.0.0/0 ......................................... lorem
+	// │  ├─ 0.0.0.0/8 ......................................... lorem
+	// │  │  └─ 0.0.0.0/10 ........................................ lorem
+	// │  ├─ 1.0.0.0/8 ......................................... lorem
+	// │  ├─ 5.0.0.0/8 ......................................... lorem
+	// │  └─ 10.0.0.0-10.0.0.17 ................................ lorem
+	// └─ ::/0 .............................................. same cidr
+	//    └─ ::/0 .............................................. same cidr, different text
+	//       ├─ ::/64 ............................................. lorem
+	//       └─ 2001:7c0:900:1c2::/64 ............................. lorem
+	//          └─ 2001:7c0:900:1c2::/127 ............................ lorem
+	//             └─ 2001:7c0:900:1c2::1/128 ........................... lorem
 
 }
